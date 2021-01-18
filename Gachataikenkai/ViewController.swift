098df85ad321252a47518e2e8c023afd8b096792
Nanaka Hayashi <nanaka_hys@icloud.com>
2021-01-18 13:59:12 +0900

    1st commit

--- Gachataikenkai/ViewController.swift
+++ Gachataikenkai/ViewController.swift
@@ -8,11 +8,37 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var gachaImageview : UIImageView!
+    @IBOutlet var gachabutton : UIButton!
+    @IBOutlet var haikeiImageview : UIImageView!
+    var number : Int!
+    
 
     override func viewDidLoad() {
+        
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        gachabutton.layer.cornerRadius = gachabutton.bounds.height/2
+        
     }
+    @IBAction func startgacha(){
+        number = Int.random(in: 0...100)
+        print(number)
+        if number < 50{
+           print("naushika")
+            gachaImageview.image = UIImage(named: "nausika.png")
+            haikeiImageview.image = UIImage(named: "sabaku.jpg")
+        }else if number > 50 && number < 90{
+            print("ponyo")
+            gachaImageview.image = UIImage(named: "ponyo.jpg")
+            haikeiImageview.image = UIImage(named: "umi.jpg")
+        }else{
+            print("rapyuta")
+            gachaImageview.image = UIImage(named: "rapyuta.jpg")
+            haikeiImageview.image = UIImage(named: "sora.jpg")
+        }
+    }
+    
 
 
 }
